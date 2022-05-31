58936bf758958d0163674450aa571eebe18516ce
moise <moisemlg90@gmail.com>
2022-05-31 12:12:37 +0200

    Add not null value

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered,weight_kg) VALUES('Agumon', '03-02-2020', 0, false, 10.23);
+
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
--- schema.sql
+++ schema.sql
@@ -1,11 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100),
-    date_of_birth DATE,
-    escape_attempts INTEGER,
-    neutered BOOLEAN,
-    weight_kg DECIMAL,
-    PRIMARY KEY(id)
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INTEGER NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
