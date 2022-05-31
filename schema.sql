3769fd088048881b97d4fa1dbad7d0b83566d466
moise <moisemlg90@gmail.com>
2022-05-31 10:45:44 +0200

    Add id value type

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    id INTEGER,
     name VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INTEGER,
