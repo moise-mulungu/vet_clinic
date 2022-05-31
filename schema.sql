6d78c3b02484a750de773138f42e72cba4cb5edb
moise <moisemlg90@gmail.com>
2022-05-31 10:47:14 +0200

    Adding identity column

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INTEGER,
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INTEGER,
